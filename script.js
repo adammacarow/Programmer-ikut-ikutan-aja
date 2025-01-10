90c3dc75a3b3ad30040b1c6293e49a817c660007
adammacarow <adamandbasri@gmail.com>
2025-01-10 15:44:53 +0800

    b

---
+++ script.js
@@ -0,0 +1,13 @@
+const aaa = 'jack';
+const bbb = 'mercy';
+const ccc = 'dva';
+const ddd = aaa + bbb + ccc;
+console.log(ddd);
+
+const h1 = document.querySelector('h1');
+h1.textContent = ddd;
+
+
+
+const myElement = document.getElementById("jack");
+myElement.style.color = "red";
